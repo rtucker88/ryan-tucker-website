9353e4c695ad37cce36ac1106fdd004f0d121c1b
Ryan Tucker <ryan.tucker@asti-usa.com>
2018-05-31 10:33:04 -0400

    Cleans up success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const SuccessPage = () => (
-  <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, libero.</div>
+  <div id="success">
+    <h1>Thanks for your submission!</h1>
+    <p>I look forward to reading your message and will get back to you soon.</p>
+  </div>
 )
 
 export default SuccessPage
