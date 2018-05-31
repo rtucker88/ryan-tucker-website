a602d3ec79dc494885c34ba5e8f2b9f7a5b09adf
Ryan Tucker <ryan.tucker@asti-usa.com>
2018-05-31 09:59:34 -0400

    Cleans up 404 pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const NotFoundPage = () => (
-  <div>
+  <div id="notFound">
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just found a route that doesn&#39;t exist...</p>
   </div>
 )
 
